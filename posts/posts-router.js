7b4e03bf0bfd6a76f0a32abe53f39cadd5f0d85b
Marissa <marissacooterl@gmail.com>
2020-08-07 15:15:50 -0500

    get post by id done

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -3,6 +3,7 @@ const db = require("../data/db")
 
 const router = express.Router()
 
+// GET ALL POSTS
 router.get("/posts", (req, res) => {
     db.find()
     .then((posts) => {
@@ -14,6 +15,26 @@ router.get("/posts", (req, res) => {
             message: "Error retrieving posts"
         })
     })  
+})
+
+// GET POSTS BY ID
+router.get("/posts/:id", (req, res) => {
+    db.findById(req.params.id)
+    .then((post) => {
+        if (post) {
+            res.status(200).json(post)
+        } else {
+            res.status(400).json({
+                message: "Post could not be found"
+            })
+        }
+    })
+    .catch((err) => {
+        console.log(err)
+        res.status(500).json({
+            message: "Error retrieving post"
+        })
+    })
 })
 
 module.exports = router
